4c865afe5645490b607e1b07ae1c372142a884a5
Hannah Lee <106423868+hannahlee2021@users.noreply.github.com>
2023-10-17 23:09:59 -0400

    commit

---
+++ js/main.js
@@ -0,0 +1,39 @@
+const day1 = {
+    time: "5:22pm",
+    num: 1,
+    texture: ["coarse", "slightly curly"],
+    color:"black",
+    hairLength: "long",
+    strandLength: "long",
+    emotionBefore: {
+        leadingEvent:{
+            location: "d12 core lab",
+            people: "classmates",
+            mood: "bored"
+        }
+    },
+    emotionDuring: {
+        awareness: false,
+        instantGratification: true
+    },
+    emotionAfter: {
+        negative: false
+    },
+    alot: function(alotOfHairs) {
+        if (this.num > 15) {
+            console.log("don't be sad it is okay");
+        }
+        else {
+            console.log("good job! not too bad this time!");
+        }
+    },
+    mood: function(bored) {
+        if (!bored) {
+            console.log("as always");
+            return;
+        }
+        console.log(bored + ", the classic mood");
+    }
+}
+
+
