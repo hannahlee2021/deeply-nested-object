f07befd4ebbe597b13075cd049d79b8ec692b743
Hannah Lee <106423868+hannahlee2021@users.noreply.github.com>
2023-10-25 10:47:57 -0400

    third commit

--- index/sketch.js
+++ index/sketch.js
@@ -4,7 +4,8 @@
 //icons
 let hair1 = [];
 let hair2 = [];
-
+let hair3 = [];
+let hair4 = [];
 
 //# of iterations per icon
 let iconNum = 20;
@@ -12,6 +13,9 @@ let iconNum = 20;
 //declaring icon as arrays (for placement)
 let icon_hair1 = [];
 let icon_hair2 = [];
+let icon_hair3 = [];
+let icon_hair4 = [];
+
 
 
 //loading + declaring image files
@@ -20,6 +24,9 @@ function preload(){
 
    icon_hair1 = loadImage('hairs/hair.png');
    icon_hair2 = loadImage('hairs/hair2.png');
+   icon_hair3 = loadImage('hairs/hair3.png');
+   icon_hair4 = loadImage('hairs/hair4.png');
+
   
   
 }
@@ -33,6 +40,8 @@ function setup() {
     
     hair1[i] = new Hair(random(windowWidth),random(windowHeight));
     hair2[i] = new Hair2(random(windowWidth), random(windowHeight));
+    hair3[i] = new Hair3(random(windowWidth), random(windowHeight));
+    hair4[i] = new Hair4(random(windowWidth), random(windowHeight));
 
       
     }
@@ -61,6 +70,13 @@ function scene0() {
 
   hair2[i].body()
   hair2[i].move()
+
+  hair3[i].body()
+  hair3[i].move()
+
+  hair4[i].body()
+  hair4[i].move()
+
  
   }
 
@@ -105,6 +121,54 @@ class Hair2 {
   body() {
     noStroke();
     image(icon_hair2, this.x, this.y, 50, 50);
+  }
+
+  move() {
+    this.y++;
+    if(this.y>height) {
+      this.y=0;
+    }
+  }
+
+  remove() {
+    this.x = -100;
+    this.y = -100;
+  }
+}
+
+class Hair3 {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  body() {
+    noStroke();
+    image(icon_hair3, this.x, this.y, 50, 50);
+  }
+
+  move() {
+    this.y++;
+    if(this.y>height) {
+      this.y=0;
+    }
+  }
+
+  remove() {
+    this.x = -100;
+    this.y = -100;
+  }
+}
+
+class Hair4 {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  body() {
+    noStroke();
+    image(icon_hair4, this.x, this.y, 50, 50);
   }
 
   move() {
