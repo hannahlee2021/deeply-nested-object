baffeda4d34c6c4bf7b510e72a3d27888daa1ef8
Hannah Lee <106423868+hannahlee2021@users.noreply.github.com>
2023-10-25 18:45:53 -0400

    fourth commit

--- js/main.js
+++ js/main.js
@@ -1,46 +1,63 @@
-const day = {
-    day1: {
-        time: "5:22pm",
-        num: 1,
-        texture: ["coarse", "slightly curly"],
-        color:"black",
-        hairLength: "long",
-        strandLength: "long",
-        emotionBefore: {
-            leadingEvent:{
-                location: "d12 core lab",
-                people: "classmates",
-                mood: "bored"
-            }
-        },
-        emotionDuring: {
-            awareness: false,
-            instantGratification: true
-        },
-        emotionAfter: {
-            negative: false
-        },
-        alot: function(alotOfHairs) {
-            if (this.num > 15) {
-                console.log("don't be sad it is okay");
-            }
-            else {
-                console.log("good job! not too bad this time!");
-            }
-        },
-        mood: function(bored) {
-            if (!bored) {
-                console.log("as always");
-                return;
-            }
-            console.log(bored + ", the classic mood");
-        }
-    },
+// const day = {
+//     day1: {
+//         time: "5:22pm",
+//         num: 1,
+//         texture: ["coarse", "slightly curly"],
+//         color:"black",
+//         hairLength: "long",
+//         strandLength: "long",
+//         emotionBefore: {
+//             leadingEvent:{
+//                 location: "d12 core lab",
+//                 people: "classmates",
+//                 mood: "bored"
+//             }
+//         },
+//         emotionDuring: {
+//             awareness: false,
+//             instantGratification: true
+//         },
+//         emotionAfter: {
+//             negative: false
+//         },
+//         alot: function(alotOfHairs) {
+//             if (this.num > 15) {
+//                 console.log("don't be sad it is okay");
+//             }
+//             else {
+//                 console.log("good job! not too bad this time!");
+//             }
+//         },
+//         mood: function(bored) {
+//             if (!bored) {
+//                 console.log("as always");
+//                 return;
+//             }
+//             console.log(bored + ", the classic mood");
+//         }
+//     },
 
-    day2: {
+//     day2: {
 
-    }
+//     }
     
-}
+// }
 
+
+const day1 = {
+    title: "day 1 [101623]",
+    time: "5:22pm",
+    num: 1,
+    texture: ["coarse, slightly curly"],
+    color: "black",
+    hairLength: "long",
+    strandLength: "long",
+    emotionBefore: {
+        leadingEvent: {
+            location: "d12",
+            people: "classmates",
+            mood: "bored"
+        }
+    }
+}
 
